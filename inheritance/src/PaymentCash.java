62b0ed27461ff8e6723c36926135415af24c5098
nugrahaindie <nugraha@indietechsolutions.com>
2025-03-07 09:32:15 +0700

    adding constructor for PaymentCash

--- inheritance/src/PaymentCash.java
+++ inheritance/src/PaymentCash.java
@@ -2,6 +2,12 @@ public class PaymentCash extends Payment {
     private String adminId;
     private String branchId;
 
+    public PaymentCash(String adminId, String branchId, String customerId, double amount) {
+        super(customerId, amount);
+        this.adminId = adminId;
+        this.branchId = branchId;
+    }
+
     public void pay(double amount) {
         System.out.println("Customer come to the branch and pay " + amount + " cash.");
     }
