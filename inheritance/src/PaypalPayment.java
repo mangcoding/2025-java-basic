6acac9dfdb6afc143c9c76e82b759216c9c01d73
nugrahaindie <nugraha@indietechsolutions.com>
2025-03-07 09:31:02 +0700

    adding more method and property

--- inheritance/src/PaypalPayment.java
+++ inheritance/src/PaypalPayment.java
@@ -2,11 +2,13 @@ public class PaypalPayment extends Payment {
     private String PaypalToken;
     private String PaypalId;
 
+    public PaypalPayment(String Token, String Id, String customerId, double amount) {
+        super(customerId, amount);
+        this.PaypalToken = Token;
+        this.PaypalId = Id;
+    }
+
     public void pay(double amount) {
         System.out.println("using paypal API to transfer " + amount + " to the account.");
     }
-
-    public void printPaymentDetails() {
-        System.out.println("Payment ID: " + this.paymentId);
-    } 
 }
